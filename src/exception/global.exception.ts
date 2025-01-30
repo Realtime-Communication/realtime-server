85eeabf627be6a96ea3d181c88cd0868e463600e
Win Nguyen <winnguyen1905.work@gmail.com>
2025-01-30 19:41:54 +0700

    global exception handler

---
+++ src/exception/global.exception.ts
@@ -0,0 +1,25 @@
+import {
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  HttpException,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import { Response } from 'express';
+
+@Catch()
+export class AllExceptionsFilter implements ExceptionFilter {
+  catch(exception: any, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+
+    if (exception instanceof HttpException) {
+      return response
+        .status(exception.getStatus())
+        .json(exception.getResponse());
+    }
+
+    console.error('Unhandled error:', exception);
+    return response.status(500).json({ message: 'Internal Server Error' });
+  }
+}
