18af068a79729f335f8d2d2f25e60060d43b3307
Win Nguyen <winnguyen1905.work@gmail.com>
2025-01-30 19:43:17 +0700

    refactor main class

--- src/main.ts
+++ src/main.ts
@@ -13,69 +13,73 @@ import { createAdapter } from '@socket.io/redis-adapter';
 import { createClient } from 'redis';
 import redisClient from './redis/redis.client';
 import { RedisIoAdapter } from './redis/redis.adapter';
-import { SwaggerModule, DocumentBuilder, SwaggerDocumentOptions } from '@nestjs/swagger';
+import {
+  SwaggerModule,
+  DocumentBuilder,
+  SwaggerDocumentOptions,
+} from '@nestjs/swagger';
 import { UsersModule } from './users/users.module';
 import { ChatModule } from './chat/realtime.module';
+import { AllExceptionsFilter } from './exception/global.exception';
 
+export class CallChatApplication {
+  public static async bootstrap(args: string[]): Promise<void> {
+    const app = await NestFactory.create<NestExpressApplication>(AppModule);
+    const configService = app.get(ConfigService);
 
+    // Redis Caching Websocket
+    const redisIoAdapter = new RedisIoAdapter(app);
+    await redisIoAdapter.connectToRedis();
+    app.useWebSocketAdapter(redisIoAdapter);
 
-async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  const configService = app.get(ConfigService);
-
-  // Redis Caching Websocket 
-  const redisIoAdapter = new RedisIoAdapter(app);
-  await redisIoAdapter.connectToRedis();
-  app.useWebSocketAdapter(redisIoAdapter);
-
-  async () => {
-    redisClient.on('error', async (err) => await redisClient.connect());
-    redisClient.on('disconnect', async () => {
-      console.log('Disconnect');
+    async () => {
+      redisClient.on('error', async (err) => await redisClient.connect());
+      redisClient.on('disconnect', async () => {
+        console.log('Disconnect');
+        await redisClient.connect();
+      });
       await redisClient.connect();
+    };
+
+    const config = new DocumentBuilder()
+      .setTitle('Talk Together')
+      .setDescription('API Description')
+      .setVersion('1.0')
+      .build();
+
+    const options: SwaggerDocumentOptions = {
+      deepScanRoutes: true,
+      ignoreGlobalPrefix: true,
+      operationIdFactory: (controllerKey: string, methodKey: string) =>
+        methodKey,
+    };
+    const document = SwaggerModule.createDocument(app, config, options);
+
+    SwaggerModule.setup('api', app, document);
+
+    app.enableCors();
+
+    // API Versioning
+    app.setGlobalPrefix('api');
+    app.enableVersioning({
+      type: VersioningType.URI,
+      defaultVersion: ['1'],
     });
-    await redisClient.connect();
+
+    // Template engine
+    app.useStaticAssets(join(__dirname, '..', 'public'));
+    app.setBaseViewsDir(join(__dirname, '..', 'views'));
+    app.setViewEngine('ejs');
+
+    // Reflector & metatdata, Guards, Interceptor
+    const reflector = app.get(Reflector);
+    app.useGlobalGuards(new RolesGuard(reflector));
+    app.useGlobalFilters(new AllExceptionsFilter());
+    app.useGlobalGuards(new JwtAuthGuard(reflector));
+    app.useGlobalInterceptors(new TransformInterceptor(reflector));
+
+    await app.listen(configService.get<string>('PORT'));
   }
-
-  const config = new DocumentBuilder()
-    .setTitle('Talk Together')
-    .setDescription('API Description')
-    .setVersion('1.0')
-    .build();
-
-  const options: SwaggerDocumentOptions = {
-    deepScanRoutes: true,
-    ignoreGlobalPrefix: true,
-    operationIdFactory: (
-      controllerKey: string,
-      methodKey: string
-    ) => methodKey
-  };
-  const document = SwaggerModule.createDocument(app, config, options);
-
-  SwaggerModule.setup('api', app, document);
-
-  app.enableCors();
-
-  // API Versioning
-  app.setGlobalPrefix('api');
-  app.enableVersioning({
-    type: VersioningType.URI,
-    defaultVersion: ['1']
-  });
-
-  // Template engine
-  app.useStaticAssets(join(__dirname, '..', 'public'));
-  app.setBaseViewsDir(join(__dirname, '..', 'views'));
-  app.setViewEngine('ejs');
-
-  // Reflector & metatdata, Guards, Interceptor
-  const reflector = app.get( Reflector );
-  app.useGlobalInterceptors(new TransformInterceptor(reflector));
-  app.useGlobalGuards( new JwtAuthGuard(reflector) );
-  app.useGlobalGuards( new RolesGuard(reflector) );
-
-  await app.listen(configService.get<string>("PORT"));
 }
 
-bootstrap();
+CallChatApplication.bootstrap(null);
